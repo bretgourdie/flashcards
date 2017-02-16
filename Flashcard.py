f79b7890a93cb0058e7206dffe030805911a8677
Bret Gourdie <bretgourdie@gmail.com>
2017-02-16 13:24:43 -0500

    Initial add of Flashcard

---
+++ Flashcard.py
@@ -0,0 +1,7 @@
+class Flashcard(object):
+
+    def __init__(self, category, term, definition, id=None):
+        self.category = category
+        self.term = term
+        self.definition = definition
+        self.id = id
